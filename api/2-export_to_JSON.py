64198f69acd8d56623764c6d75b3b0f55d23b7af
MURARA Kenny Roger <R.murara@alustudent.com>
2023-11-10 07:02:10 +0200

    c2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""gathers data from an API"""
+"""get TODO list"""
 
+import csv
 import json
 import requests
 import sys
-
-"""a Python script to export data in the JSON format"""
-
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
-    todo = todo.format(employee_id)
-
-    user_info = requests.request("GET", url).json()
-    todo_info = requests.request("GET", todo).json()
-
-    employee_username = user_info.get("username")
-
-    todos_info_sorted = [
-        dict(zip(["task", "completed", "username"],
-                 [task["title"], task["completed"], employee_username]))
-        for task in todo_info]
-
-    user_dict = {str(employee_id): todos_info_sorted}
-    with open(str(employee_id) + '.json', "w") as f:
-        f.write(json.dumps(user_dict))
+    link = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+    res = requests.get(link)
+    user = json.loads(res.text)
+    num = sys.argv[1]
+    link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
+    res = requests.get(link)
+    todos = json.loads(res.text)
+    data = [{"task": i["title"],
+             "completed": i["completed"],
+             "username": user["username"]} for i in todos]
+    json_data = json.dumps({"{}".format(user["id"]): data})
+    with open("{}.json".format(user["id"]), 'w', encoding='utf-8') as f:
+        f.write(json_data)
