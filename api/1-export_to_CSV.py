1783c7e7e8995e4563fef5be51d0dc5dcb542430
MURARA Kenny Roger <R.murara@alustudent.com>
2023-11-10 06:42:12 +0200

    c1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Python script that exports data in the CSV format
+    python script that exports data in the CSV format
 """
 import csv
 import json
@@ -10,44 +10,42 @@ from sys import argv
 
 if __name__ == "__main__":
     """
-        Request user info by employee ID
+        request user info by employee ID
     """
     request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
     """
-        Convert json to dictionary
+        convert json to dictionary
     """
     user = json.loads(request_employee.text)
     """
-        Extract username
+        extract username
     """
     username = user.get("username")
 
     """
-        Request user's TODO list
+        request user's TODO list
     """
     request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
     """
-        Dictionary to store task status(completed) in boolean format
+        dictionary to store task status(completed) in boolean format
     """
     tasks = {}
     """
-        Convert json to list of dictionaries
+        convert json to list of dictionaries
     """
     user_todos = json.loads(request_todos.text)
     """
-        Loop through dictionary & get completed tasks
+        loop through dictionary & get completed tasks
     """
     for dictionary in user_todos:
         tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
     """
-        Export to CSV
+        export to CSV
     """
-    with open('{}.csv'.format(argv[1]), mode='w', newline='') as file:
+    with open('{}.csv'.format(argv[1]), mode='w') as file:
         file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
         for k, v in tasks.items():
             file_editor.writerow([argv[1], username, v, k])
-
-    print('Data exported to {}.csv'.format(argv[1]))
